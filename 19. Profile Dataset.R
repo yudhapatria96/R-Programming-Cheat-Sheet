5579d716ea376e9a8bee4ce16ac30a613792503d
Yudha Patria <yudhapatria96@gmail.com>
2021-05-15 10:44:56 +0700

    penambahan beberapa materi

---
+++ 19. Profile Dataset.R
@@ -0,0 +1,6 @@
+kasus.corona <- read.csv("https://drive.google.com/uc?id=1PvWG8vMo0UlWnvEyDXamOHYOTjUmosxe&export=download", sep=",")
+
+
+str(kasus.corona)
+
+summary(kasus.corona)
---
+++ 20. Membaca File Excel (Lokal dan Internet).R
@@ -0,0 +1,6 @@
+install.packages("openxlsx")
+
+library(openxlsx)
+
+info.corona <- read.xlsx("https://drive.google.com/u/6/uc?id=1WQ3Kv3XH_U1fun560JFKaiibv1oy9qm6&export=download")
+info.corona
